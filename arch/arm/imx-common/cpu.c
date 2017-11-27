e92a9b4a545594d8127ddd34c23836e1c2bc01ba
Aditya Paluri <venkataaditya.paluri@in.bosch.com>
2017-11-27 19:13:06 +0530

    self: Disabled splash screen reset
    Signed-off-by: Aditya Paluri <pv.aditya@gmail.com>

--- arch/arm/imx-common/cpu.c
+++ arch/arm/imx-common/cpu.c
@@ -279,7 +279,7 @@ void arch_preboot_os(void)
 #endif
 #if defined(CONFIG_VIDEO_IPUV3)
 	/* disable video before launching O/S */
-	ipuv3_fb_shutdown();
+//	ipuv3_fb_shutdown();
 #endif
 #if defined(CONFIG_VIDEO_MXS)
 	lcdif_power_down();
